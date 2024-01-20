196b3c86c671989188ca16272b62c2da60d10156
gerald-wambui <geraldwainaina25@gmail.com>
2024-01-20 22:05:51 +0300

    initial gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,26 @@ dependencies {
 	androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 	debugImplementation("androidx.compose.ui:ui-tooling")
 	debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+	implementation("androidx.navigation:navigation-compose:2.7.6")
+
+	implementation("androidx.room:room-ktx:2.6.1")
+	kapt("androidx.room:room-compiler:2.6.1")
+	implementation("androidx.room:room-paging:2.6.1")
+
+	implementation("com.google.dagger:hilt-android:2.48")
+	kapt("com.google.dagger:hilt-compiler:2.44")
+	kapt("androidx.hilt:hilt-compiler:1.1.0")
+	implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+	implementation("com.squareup.retrofit2:retrofit:2.9.0")
+	implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+	implementation("com.squareup.okhttp3:okhttp:4.11.0")
+	implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
+	implementation("io.coil-kt:coil-compose:2.4.0")
+
+	implementation("androidx.compose.material:material-icons-extended:1.5.4")
+
+	implementation("com.google.accompanist:accompanist-systemuicontroller:0.27.0")
 }
