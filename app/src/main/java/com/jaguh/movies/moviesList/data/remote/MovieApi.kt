dc2979c81a79c6044b14fcab5e267a54c6f202bd
gerald-wambui <geraldwainaina25@gmail.com>
2024-01-21 12:37:13 +0300

    movie api

--- app/src/main/java/com/jaguh/movies/moviesList/data/remote/MovieApi.kt
+++ app/src/main/java/com/jaguh/movies/moviesList/data/remote/MovieApi.kt
@@ -1,5 +1,6 @@
 package com.jaguh.movies.moviesList.data.remote
 
+import com.jaguh.movies.moviesList.data.remote.respond.MovieListDto
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -11,7 +12,7 @@ interface MovieApi {
 		@Path("category") category: String,
 		@Query("page") page: Int,
 		@Query("api_key") apiKey: String = API_KEY
-	)
+	): MovieListDto
 
 	companion object {
 		const val BASE_URL = "https://api.themoviedb.org/3/"
