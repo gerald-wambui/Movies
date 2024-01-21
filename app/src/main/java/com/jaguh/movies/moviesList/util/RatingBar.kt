30283fc2dfeb0cd4aa5b9ad788fcda79c28e10e3
gerald-wambui <geraldwainaina25@gmail.com>
2024-01-21 05:16:21 +0300

    utils

---
+++ app/src/main/java/com/jaguh/movies/moviesList/util/RatingBar.kt
@@ -0,0 +1,16 @@
+package com.jaguh.movies.moviesList.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun RatingBar(
+	modifier: Modifier = Modifier,
+	startsModifier: Modifier = Modifier,
+	rating: Double = 0.0,
+	stars: Int = 5,
+	starsColor: Color = Color.Yellow
+	) {
+
+}
