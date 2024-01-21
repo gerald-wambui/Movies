0da492d8d594ffee604db390d3e8ed69b37d1da3
gerald-wambui <geraldwainaina25@gmail.com>
2024-01-21 08:26:30 +0300

    mappers

--- app/src/main/java/com/jaguh/movies/moviesList/data/mappers/MovieMapper.kt
+++ app/src/main/java/com/jaguh/movies/moviesList/data/mappers/MovieMapper.kt
@@ -7,6 +7,23 @@ fun MovieEntity.toMovie(
 	category: String
 ): Movie {
 	return Movie(
-		backdrop_path = backdrop_path
+		backdrop_path = backdrop_path,
+		original_language = original_language,
+		overview = overview,
+		poster_path = poster_path,
+		release_date = release_date,
+		title = title,
+		vote_average = vote_average,
+		popularity = popularity,
+		vote_count = vote_count,
+		video = video,
+		id = id,
+		adult = adult,
+		original_title = original_title,
+		category = category,
+
+		genre_ids = try {
+
+		}
 	)
 }
