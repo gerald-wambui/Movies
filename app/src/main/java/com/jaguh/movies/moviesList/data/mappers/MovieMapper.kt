82eadac26c838b66a15688c08e22ac4ec02b3962
gerald-wambui <geraldwainaina25@gmail.com>
2024-01-21 08:38:53 +0300

    mappers

--- app/src/main/java/com/jaguh/movies/moviesList/data/mappers/MovieMapper.kt
+++ app/src/main/java/com/jaguh/movies/moviesList/data/mappers/MovieMapper.kt
@@ -1,7 +1,40 @@
 package com.jaguh.movies.moviesList.data.mappers
 
 import com.jaguh.movies.moviesList.data.local.movie.MovieEntity
+import com.jaguh.movies.moviesList.data.remote.respond.MovieDto
 import com.jaguh.movies.moviesList.domain.model.Movie
+
+
+fun MovieDto.toMovieEntity(
+	category: String
+): MovieEntity {
+	return MovieEntity(
+		adult = adult ?: false,
+		backdrop_path = backdrop_path ?: "",
+		original_language = original_language ?: "",
+		overview = overview ?: "",
+		poster_path = poster_path ?: "",
+		release_date = release_date ?: "",
+		title = title ?: "",
+		vote_average = vote_average ?: 0.0,
+		popularity = popularity ?: 0.0,
+		vote_count = vote_count ?: 0,
+		id = id ?: -1,
+		original_title = original_title ?: "",
+		video = video ?: false,
+
+		category = category,
+
+		genre_ids = try{
+			genre_ids?.joinToString(",") ?: "-1,-2"
+		} catch (e: Exception) {
+			"-1,-2"
+		}
+	)
+}
+
+
+
 
 fun MovieEntity.toMovie(
 	category: String
@@ -23,7 +56,7 @@ fun MovieEntity.toMovie(
 		category = category,
 
 		genre_ids = try {
-
+			genre_ids.split(",")
 		}
 	)
 }
