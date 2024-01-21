001a8e76b00208034ea869744d9e4e346883e75f
gerald-wambui <geraldwainaina25@gmail.com>
2024-01-21 05:22:12 +0300

    utils

--- app/src/main/java/com/jaguh/movies/moviesList/util/RatingBar.kt
+++ app/src/main/java/com/jaguh/movies/moviesList/util/RatingBar.kt
@@ -1,5 +1,9 @@
 package com.jaguh.movies.moviesList.util
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Star
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -7,10 +11,26 @@ import androidx.compose.ui.graphics.Color
 @Composable
 fun RatingBar(
 	modifier: Modifier = Modifier,
-	startsModifier: Modifier = Modifier,
+	starsModifier: Modifier = Modifier,
 	rating: Double = 0.0,
 	stars: Int = 5,
 	starsColor: Color = Color.Yellow
 	) {
 
+	val filledStars = kotlin.math.floor(rating).toInt()
+	val unfilledStars = (stars - kotlin.math.ceil(rating)).toInt()
+	val halfStar = !(rating.rem(1).equals(0.0))
+
+	Row(modifier = modifier) {
+		repeat(filledStars) {
+			Icon(
+				modifier = starsModifier,
+				imageVector = Icons.Rounded.Star,
+				contentDescription = null,
+				tint = starsColor
+			)
+		}
+
+		if ()
+	}
 }
