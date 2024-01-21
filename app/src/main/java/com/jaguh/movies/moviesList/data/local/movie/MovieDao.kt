878ab446cf1b8fa3c47d95d536194b03969605f3
gerald-wambui <geraldwainaina25@gmail.com>
2024-01-21 04:34:45 +0300

    data layer

---
+++ app/src/main/java/com/jaguh/movies/moviesList/data/local/movie/MovieDao.kt
@@ -0,0 +1,19 @@
+package com.jaguh.movies.moviesList.data.local.movie
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+
+
+@Dao
+interface MovieDao {
+
+	@Upsert
+	suspend fun upsertMovieList(movieList: List<MovieEntity>)
+
+	@Query("SELECT * FROM MovieEntity WHERE id = :id")
+	suspend fun getMovieById(id: Int): MovieEntity
+
+	@Query("SELECT * FROM MovieEntity WHERE category = :category")
+	suspend fun getMovieListByCategory(category: String): List<MovieEntity>
+}
