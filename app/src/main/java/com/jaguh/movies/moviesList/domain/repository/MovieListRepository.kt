51fdf707ce76e8b41a2668c5848bb4fef2c9b062
gerald-wambui <geraldwainaina25@gmail.com>
2024-01-21 05:28:28 +0300

    movie repository

--- app/src/main/java/com/jaguh/movies/moviesList/domain/repository/MovieListRepository.kt
+++ app/src/main/java/com/jaguh/movies/moviesList/domain/repository/MovieListRepository.kt
@@ -1,6 +1,8 @@
 package com.jaguh.movies.moviesList.domain.repository
 
-import java.util.concurrent.Flow
+import com.jaguh.movies.moviesList.domain.model.Movie
+import com.jaguh.movies.moviesList.util.Resource
+import kotlinx.coroutines.flow.Flow
 
 interface MovieListRepository {
 
@@ -8,5 +10,5 @@ interface MovieListRepository {
 		forceFetchFromRemote: Boolean,
 		category: String,
 		page: Int
-	): Flow<>
+	): Flow<Resource<List<Movie>>>
 }
