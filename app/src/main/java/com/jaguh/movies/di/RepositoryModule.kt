5d75eeb2b3c56cdf1e49f1cee044744b82f3c819
gerald-wambui <geraldwainaina25@gmail.com>
2024-01-21 18:32:59 +0300

    RepositoryModule

--- app/src/main/java/com/jaguh/movies/di/RepositoryModule.kt
+++ app/src/main/java/com/jaguh/movies/di/RepositoryModule.kt
@@ -1,4 +1,20 @@
 package com.jaguh.movies.di
 
-class RepositoryModule {
+import com.jaguh.movies.moviesList.data.repository.MovieListRepositoryImpl
+import com.jaguh.movies.moviesList.domain.repository.MovieListRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+	@Binds
+	@Singleton
+	abstract fun bindMovieListRepository(
+		movieListRepositoryImpl: MovieListRepositoryImpl
+	): MovieListRepository
 }
