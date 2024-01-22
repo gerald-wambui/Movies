3600b3cb9a5a3205a81660f8a69bc44aa2b47141
gerald-wambui <geraldwainaina25@gmail.com>
2024-01-22 17:04:30 +0300

    core

--- app/src/main/java/com/jaguh/movies/core/presentation/HomeScreen.kt
+++ app/src/main/java/com/jaguh/movies/core/presentation/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.jaguh.movies.core.presentation
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -12,6 +13,7 @@ import androidx.navigation.compose.rememberNavController
 import com.jaguh.movies.moviesList.presentation.MovieListViewModel
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavHostController) {
 
