5f1297bc0947b39c057cd314fd87b709023de882
gerald-wambui <geraldwainaina25@gmail.com>
2024-01-22 06:54:14 +0300

    presentation

--- app/src/main/java/com/jaguh/movies/moviesList/presentation/MovieListState.kt
+++ app/src/main/java/com/jaguh/movies/moviesList/presentation/MovieListState.kt
@@ -1,5 +1,18 @@
 package com.jaguh.movies.moviesList.presentation
 
+import com.jaguh.movies.moviesList.domain.model.Movie
+
 data class MovieListState(
-	val isLoading: Boolean = false
+
+	val isLoading: Boolean = false,
+
+	val popularMovieListPage: Int = 1,
+
+	val upcomingMovieListPage: Int = 1,
+
+	val isCurrentPopularScreen: Boolean = true,
+
+	val popularMovieList: List<Movie> = emptyList(),
+
+	val upcomingMovieList: List<Movie> = emptyList()
 )
