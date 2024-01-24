3dd98334181a8a8c0658a5dfdfcb0927b66db1d5
gerald-wambui <geraldwainaina25@gmail.com>
2024-01-24 17:24:56 +0300

    home screen

--- app/src/main/java/com/jaguh/movies/core/presentation/HomeScreen.kt
+++ app/src/main/java/com/jaguh/movies/core/presentation/HomeScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableIntStateOf
@@ -87,8 +88,15 @@ fun BottomNavigationBar(
 					icon = {
 						Icon(
 							imageVector = bottomItem.icon,
-							contentDescription = bottomItem.title
+							contentDescription = bottomItem.title,
+							tint = MaterialTheme.colorScheme.onBackground
 						)
+					},
+					label = {
+						Text(
+							text = bottomItem.title,
+							color = MaterialTheme.colorScheme.onBackground
+							)
 					}
 				)
 			}
