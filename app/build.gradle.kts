18a1cb85441acf22ed7bd7cef65d2ea373fb1c13
gerald-wambui <geraldwainaina25@gmail.com>
2024-01-24 22:19:31 +0300

    Migrate to KSP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,8 @@ plugins {
 	id("com.android.application")
 	id("org.jetbrains.kotlin.android")
 	id("com.google.dagger.hilt.android")
-	id("kotlin-kapt")
+	id("com.google.devtools.ksp")
+	//id("kotlin-kapt")
 }
 
 android {
@@ -12,7 +13,6 @@ android {
 	defaultConfig {
 		applicationId = "com.jaguh.movies"
 		minSdk = 21
-		//noinspection EditedTargetSdkVersion
 		targetSdk = 34
 		versionCode = 1
 		versionName = "1.0"
@@ -73,12 +73,13 @@ dependencies {
 	implementation("androidx.navigation:navigation-compose:2.7.6")
 
 	implementation("androidx.room:room-ktx:2.6.1")
-	kapt("androidx.room:room-compiler:2.6.1")
+	//kapt("androidx.room:room-compiler:2.6.1")
+	ksp("androidx.room:room-compiler:2.6.1")
 	implementation("androidx.room:room-paging:2.6.1")
 
 	implementation("com.google.dagger:hilt-android:2.48")
-	kapt("com.google.dagger:hilt-compiler:2.44")
-	kapt("androidx.hilt:hilt-compiler:1.1.0")
+	ksp("com.google.dagger:hilt-compiler:2.44")
+	ksp("androidx.hilt:hilt-compiler:1.1.0")
 	implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
 	implementation("com.squareup.retrofit2:retrofit:2.9.0")
