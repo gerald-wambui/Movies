82d9a461d61462be635c3e46db89fdf55bf01042
gerald-wambui <geraldwainaina25@gmail.com>
2024-01-24 17:07:25 +0300

    home screen

--- app/src/main/java/com/jaguh/movies/core/presentation/HomeScreen.kt
+++ app/src/main/java/com/jaguh/movies/core/presentation/HomeScreen.kt
@@ -2,14 +2,20 @@ package com.jaguh.movies.core.presentation
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Movie
+import androidx.compose.material.icons.rounded.Upcoming
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.jaguh.movies.moviesList.presentation.MovieListUiEvent
 import com.jaguh.movies.moviesList.presentation.MovieListViewModel
 
 
@@ -38,3 +44,47 @@ fun HomeScreen(navController: NavHostController) {
 	}
 
 }
+
+
+@Composable
+fun BottomNavigationBar(
+	bottomNavController: NavHostController,
+	onEvent: (MovieListUiEvent) -> Unit
+) {
+
+	val items = listOf(
+		BottomItem(
+			title = "Popular",
+			icon = Icons.Rounded.Movie
+		),
+		BottomItem(
+			title = "Upcoming",
+			icon = Icons.Rounded.Upcoming
+		)
+	)
+}
+
+
+data class BottomItem(
+	val title: String,
+	val icon: ImageVector
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
